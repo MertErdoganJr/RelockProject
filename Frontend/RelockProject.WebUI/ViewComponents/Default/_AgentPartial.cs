0e8734031d998628709f79df77920c7db036fa93
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-06-16 20:36:06 +0300

    add_main_page

---
+++ Frontend/RelockProject.WebUI/ViewComponents/Default/_AgentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RelockProject.WebUI.ViewComponents.Default
+{
+    public class _AgentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
---
+++ Frontend/RelockProject.WebUI/ViewComponents/Default/_PropertyListPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RelockProject.WebUI.ViewComponents.Default
+{
+    public class _PropertyListPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
